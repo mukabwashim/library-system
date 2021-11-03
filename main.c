0ba28cfdb9407027810a94ef1f7b95ea0b38da5d
mukabwashim <90699046+mukabwashim@users.noreply.github.com>
2021-11-03 13:22:59 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+   Project:  County Library System
+   Author:  Mukabwa Chelsea Shim
+   Date:     Oct, 2021
+   Compiler: GNC GCC
+   Language: C 99
+   License:  MIT
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCounty Library System\n");
+    printf("Welcome Ms. Elizabeth. \n");
+    printf("What would you like to do?");
+    printf("1. Add new User. \n");
+    printf("2. Add Book. \n");
+    printf("4. Search Book. \n");
+    printf("3. Borrow Book. \n");
+    printf("4. Return Book. \n");
+    return 0;
+}
+
